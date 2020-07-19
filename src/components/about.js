f1b5684661fed26abe8a34f58d8925385dcb317c
jkidune <kidunejoseph91@gmail.com>
2020-07-19 12:26:23 +0300

    commit on 'styling'

--- src/components/about.js
+++ src/components/about.js
@@ -10,6 +10,7 @@ function About() {
            <NavBar/>
            <div>
                <h1>DO YOU WANT TO KNOW MORE?</h1>
+               <p className="aboutme">My name is joseph Masonda, and this is my first app.<br>I think this is a begining of something great.</br></p>
            </div>
         </div>
     )
